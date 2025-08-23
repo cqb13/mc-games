0737f9212be1768f1ca945086295eff675c57f6d
cqb13 <cqb13.main@gmail.com>
2025-08-23 10:13:47 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# MC Games
+
+Play some games while your traveling, getting started in a new world, or just bored.
+
+## How to Use
+
+## Features
+
+## TODO:
+
+### Chat
+
+- Hangman
+- Wordle
+- Tic-Tac-Toe
+
+### World
+
+**Less time = More Points**
+
+- Scavenger Hunt
+    - Difficulty Modes
+    - Dimension Mode
+    - Collect X types of items
+    - Collect X amount of each type
+- Getting Started
+    - Equip a full suit of diamond armor
+    - Have a diamond sword, pic, and axe
+    - Have a stack of smelted iron & a stack of diamonds
+    - Have a stack of any food
+    - Have 2 stacks of cobble stone
+- Navigator
+    - Get to a destination (tracks time)
---
+++ src/main/java/dev/cqb13/McGames/McGames.java
@@ -0,0 +1,49 @@
+package dev.cqb13.McGames;
+
+import dev.cqb13.McGames.commands.CommandExample;
+import dev.cqb13.McGames.hud.HudExample;
+import dev.cqb13.McGames.modules.ModuleExample;
+import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.addons.GithubRepo;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.commands.Commands;
+import meteordevelopment.meteorclient.systems.hud.Hud;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import org.slf4j.Logger;
+
+public class McGames extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
+    public static final Category CATEGORY = new Category("Example");
+    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing Meteor Addon Template");
+
+        // Modules
+        Modules.get().add(new ModuleExample());
+
+        // Commands
+        Commands.add(new CommandExample());
+
+        // HUD
+        Hud.get().register(HudExample.INFO);
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
+
+    @Override
+    public String getPackage() {
+        return "dev.cqb13.McGames";
+    }
+
+    @Override
+    public GithubRepo getRepo() {
+        return new GithubRepo("cqb13", "mc-games");
+    }
+}
